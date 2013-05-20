f9463a22f4e4d2c790a18480879cd92fb90d3848
Chethana <cpaniyad@uci.edu>
2013-05-20 01:57:32 -0700

    c

--- core/src/edu/uci/opim/core/node/NodeState.java
+++ core/src/edu/uci/opim/core/node/NodeState.java
@@ -2,4 +2,11 @@ package edu.uci.opim.core.node;
 
 public class NodeState {
 
+	public final String id;
+	public final String name;
+
+	public NodeState(String ID, String stateName) {
+		this.id = ID;
+		this.name = stateName;
+	}
 }
