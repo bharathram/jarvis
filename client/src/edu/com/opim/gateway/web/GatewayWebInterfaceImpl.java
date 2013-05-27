2a816a78f4e5b87277592f5694ff3c7066aa7720
Chetna <cpaniyad@uci.edu>
2013-05-26 23:42:42 -0700

    Java 1.5 compliance

--- client/src/edu/com/opim/gateway/web/GatewayWebInterfaceImpl.java
+++ client/src/edu/com/opim/gateway/web/GatewayWebInterfaceImpl.java
@@ -4,23 +4,22 @@ import edu.uci.opim.node.NodeState;
 
 public class GatewayWebInterfaceImpl implements GatewayWebInterface {
 
-	@Override
 	public Boolean actionOnNode(String node, NodeState newState,
 			Object parameter) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Integer actionOnClass(String nClass, NodeState newState,
 			Object parameter) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public NodeState getState(String node) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 }
