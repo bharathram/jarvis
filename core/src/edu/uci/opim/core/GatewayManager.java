f488b4714c7e5890b369be73b7e83d75ced9ca7f
bram <bpattabh@uci.edu>
2013-05-27 14:37:08 -0700

    Gateway Manager interface

---
+++ core/src/edu/uci/opim/core/GatewayManager.java
@@ -0,0 +1,11 @@
+package edu.uci.opim.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.uci.opim.core.web.GatewayNode;
+
+public class GatewayManager {
+	private List<GatewayNode> gatewayList = new ArrayList<GatewayNode>();
+
+}
