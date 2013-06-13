cf013db6729553a366792c65bf3849aaa439fa83
bram <bpattabh@uci.edu>
2013-06-12 19:49:03 -0700

    Integrating the light and temperature sensors
    Signed-off-by: bram <bpattabh@uci.edu>

--- modules/src/edu/uci/jarvis/light/LightSensor.java
+++ modules/src/edu/uci/jarvis/light/LightSensor.java
@@ -1,7 +1,9 @@
 package edu.uci.jarvis.light;
 
+import net.xeoh.plugins.base.annotations.PluginImplementation;
 import edu.uci.jarvis.applet.SensorSimMod;
 
+@PluginImplementation
 public class LightSensor extends SensorSimMod {
 
 	/**
@@ -9,13 +11,6 @@ public class LightSensor extends SensorSimMod {
 	 */
 
 	public LightSensor() {
-		// TODO Auto-generated constructor stub
-		super("Earthquake Sensor", new String[] { "Light on", "Light off" });
+		super("Light Sensor", new String[] { "Light on", "Light off" });
 	}
-
-	public static void main(String[] args) {
-		LightSensor sen = new LightSensor();
-
-	}
-
 }
